9736b948df3716cdecd9cba30b847aff03a99ff2
Collins Mwangi <mwangicollins391@gmail.com>
2026-02-10 03:26:29 +0300

    Added robots.txt and fixed wramgler config

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+    site: 'https://collinsmwangi.me',
     output: 'server',
-    integrations: [svelte()],
+    integrations: [svelte(), sitemap()],
     adapter: cloudflare(),
     vite: {
         plugins: [tailwindcss()]

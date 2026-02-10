aee140da38e7b00adfca47a8695561585cba7a80
Collins Mwangi <mwangicollins391@gmail.com>
2026-02-10 13:36:05 +0300

    Transitioned from SSR to Static for latency purposes(even if it is boring)

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -1,15 +1,12 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: 'https://collinsmwangi.me',
-    output: 'server',
-    integrations: [svelte(), sitemap()],
-    adapter: cloudflare(),
+    output: 'static',
+    integrations: [sitemap()],
     vite: {
         plugins: [tailwindcss()]
     }

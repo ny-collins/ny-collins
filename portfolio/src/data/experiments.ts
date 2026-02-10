00b391a0def9f1e8630462583f063889699e98a1
Collins Mwangi <mwangicollins391@gmail.com>
2026-02-10 18:15:31 +0300

    Fixed Accessibility issues on new 'Engineering' section

---
+++ portfolio/src/data/experiments.ts
@@ -0,0 +1,10 @@
+// src/data/experiments.ts
+export const experiments = [
+    {
+        title: "IEEE 754 Visualizer",
+        url: "/notebook/ieee-754",
+        desc: "Interactive exploration of floating-point memory representation in the IEEE-754 Standard. Toggle bits to see how computers store decimals.",
+        tags: ["Svelte", "Systems", "Binary"],
+        date: "2026-02-10"
+    }
+];
